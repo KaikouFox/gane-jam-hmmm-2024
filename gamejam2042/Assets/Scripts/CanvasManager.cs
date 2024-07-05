4a8256407ba8fccea72fa87ed5ecd1432d13118f
KaikouFox <cacauboon.vangember@gmail.com>
2024-07-05 11:58:41 +0200

    assets added

---
+++ gamejam2042/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CanvasManager : MonoBehaviour
+{
+    public enum CanvasState
+    {
+        InGame,
+    }
+
+    public enum BarType
+    {
+        
+    }
+
+    [SerializeField] private GameObject actionText;
+    [SerializeField] private List<GameObject> screens;
+
+    [Header("Sliders")]
+
+    private CanvasState currentState;
+
+    public void SetCanvasState(CanvasState state)
+    {
+        currentState = state;
+        foreach (GameObject screen in screens)
+        {
+            screen.SetActive(false);
+        }
+
+        switch (state)
+        {
+            case CanvasState.InGame:
+                screens[0].SetActive(true);
+                break;
+        }
+    }
+
+    public void SetMaxBarValue(float newValue, BarType type)
+    {
+        switch (type)
+        {
+            
+        }
+    }
+
+    public void SetBarValue(float newValue, BarType type)
+    {
+        switch (type)
+        {
+            
+        }
+    }
+
+    public void SetActionPoints(int amount)
+    {
+        actionText.GetComponent<TextMeshProUGUI>().text = "Actionpoints: " + amount;
+    }
+}
---
+++ gamejam2042/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private int actionPoints = 10;
+
+    private float movementX;
+    private float movementY;
+    private CanvasManager canvasManager;
+    private bool touchScrap = false;
+    private GameObject scrap;
+    private int scrapAmount = 0;
+
+    private void Start()
+    {
+        canvasManager = FindObjectOfType<CanvasManager>();
+        canvasManager.SetActionPoints(actionPoints);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && touchScrap)
+        {
+            ChangePointAmount(-1);
+            scrapAmount += 1;
+            Destroy(scrap);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        movementY = Input.GetAxisRaw("Vertical");
+
+        transform.position = transform.position + new Vector3(movementX * baseSpeed * Time.deltaTime, movementY * baseSpeed * Time.deltaTime, 0);
+    }
+
+    public void ChangePointAmount(int newValue)
+    {
+        actionPoints += newValue;
+        canvasManager.SetActionPoints(actionPoints);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("scrap"))
+        {
+            touchScrap = true;
+            scrap = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("scrap"))
+        {
+            touchScrap = false;
+        }
+    }
+}
